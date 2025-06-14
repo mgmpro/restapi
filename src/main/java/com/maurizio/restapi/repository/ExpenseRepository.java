c7034552519c2ef5d5ba224f94edac71243cbf9e
mgmpro <mauriziogarciamelo@gmail.com>
2025-06-14 02:36:22 -0400

    Revert "completed the read expenses api"
    This reverts commit f80ca28e8f759cb39624b6924e35b37b3b738cac.

--- src/main/java/com/maurizio/restapi/repository/ExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.maurizio.restapi.repository;
-
-import com.maurizio.restapi.entity.ExpenseEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
-}
